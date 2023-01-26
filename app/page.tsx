a321210f7ba9f844995cbf47984d2bdb5d427876
Keaton <keatonwyd@gmail.com>
2023-01-25 22:58:53 -0500

    animate page up

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 
   return (
     <div className="flex w-screen h-screen max-sm:h-fit flex-col py-2">
-      <div className="h-32 max-w-12 animate-slide-down font-monospace">
+      <div className="h-32 max-w-12 animate-slide-up font-monospace">
         <div className="absolute inline-flex top-5 left-10 bg-gray-200/50 drop-shadow-md px-3 py-1 rounded-lg border border-gray-300">
           <h1>Keaton M.</h1>
         </div>
@@ -31,22 +31,22 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col items-center px-20 py-20 text-center">
-        <h1 className="text-3xl font-semibold animate-slide-down">
+        <h1 className="text-3xl font-semibold animate-slide-up">
           introduction
         </h1>
 
-        <h1 className="mt-3 lg:max-w-2xl animate-slide-down font-monospace">
+        <h1 className="mt-3 lg:max-w-2xl animate-slide-up font-monospace">
           Hi, I&#39;m Keaton. I am a self-taught software engineer from Florida.
           I am currently pursuing full-stack web development to create stunning
           user experiences on the front-end, and scalable and secure
           infrastructure on the backend.
         </h1>
 
-        <h1 className="mt-10 font-semibold text-3xl animate-slide-down">
+        <h1 className="mt-10 font-semibold text-3xl animate-slide-up">
           experience
         </h1>
 
-        <div className="flex flex-col items-center justify-center text-center max-sm:w-96 p-1 mt-5 animate-slide-down">
+        <div className="flex flex-col items-center justify-center text-center max-sm:w-96 p-1 mt-5 animate-slide-up">
           <Job
             title="Software Engineer @ Fractal"
             when="January 2023 - Present"
@@ -59,11 +59,11 @@ export default function Home() {
           />
         </div>
 
-        <h1 className="mt-10 font-semibold text-3xl animate-slide-down">
+        <h1 className="mt-10 font-semibold text-3xl animate-slide-up">
           how i do it
         </h1>
 
-        <div className="flex flex-wrap flex-row justify-center p-1 rounded-md bg-white/20 mt-5 mb-12 animate-slide-down">
+        <div className="flex flex-wrap flex-row justify-center p-1 rounded-md bg-white/20 mt-5 mb-12 animate-slide-up">
           <Technology icon={SiReact} />
           <Technology icon={SiTypescript} />
           <Technology icon={SiNextdotjs} />
